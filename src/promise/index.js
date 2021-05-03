bac415887ff855506c82bf833daf73592dad2932
Christiano0407 <placebo0704@gmail.com>
2021-05-03 16:30:37 -0500

    promises

--- src/promise/index.js
+++ src/promise/index.js
@@ -14,3 +14,28 @@ const somethingWillHappen = () => {
 somethingWillHappen()
 .then(Response => console.log(Response))
 .catch(error => console.error( error)); 
+
+                     /*Two*/
+const somethingHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if(true) {
+            setTimeout(() => {
+                resolve(`true!`); 
+            }, 2000)
+        }else {
+            const error = new Error(`NO!!!`);
+            reject(error); 
+        }
+    })
+}
+
+somethingHappen2()
+.then(response => console.log(response))
+.catch(error => console.error(error)); 
+
+                 /*Ejecutar todas las promesas*/
+
+Promise.all([somethingWillHappen(), somethingHappen2()])
+.then(response => {console.log(`Array`, response); 
+})
+.catch(err => { console.error(err)})
