65ceb7fe876920fe042145dc9377c856017c8b32
Christiano0407 <placebo0704@gmail.com>
2021-05-03 13:06:19 -0500

    node

---
+++ src/callback/index.js
@@ -0,0 +1,24 @@
+function sum(num1, num2) {
+   return num1 + num2; 
+}
+
+function calc(num1, num2, callback) {
+    return callback(num1, num2); 
+}
+
+console.log(calc(5, 10, sum)); 
+
+/*  TWo */
+
+function date(callback) {
+    console.log(new Date); 
+    setTimeout(function () {
+        let date = new Date;
+        callback(date) 
+    }, 2000); 
+}
+
+function princeDate(dateNow) {
+    console.log(dateNow);
+}
+date(princeDate); 
