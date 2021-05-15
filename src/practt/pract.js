8ecc67c9c0dc92c96db5e55da36220732ec7ec79
Christiano0407 <placebo0704@gmail.com>
2021-05-15 10:45:28 -0500

    twoBucle_practice

--- src/practt/pract.js
+++ src/practt/pract.js
@@ -1,41 +1,92 @@
 console.log("Hello World"); 
 
-// Bucles - Js
+// Bucles  1)======-Do Js
 
-function bucleWhile(num) {
+/* function bucleWhile(num) { */
+/*  */
+/*     let i = 0;  */
+/*  */
+/*     while(i <= num) { */
+/*      console.log(i);  */
+/*      i++;  */
+/*     } */
+/* } */
+/* bucleWhile(15);  */
+/*  */
+/* console.log("Two");  */
+/*  */
+/* twoBucle = (data) => { */
+/*   */
+/*     let i = 5;  */
+/*  */
+/*     while(i < data) { */
+/*         console.log(i);  */
+/*         i++;  */
+/*     } */
+/* } */
+/* twoBucle(15);  */
+/*  */
+/* console.log("Three");  */
+/*  */
+/* threeBucle = (numeros) => { */
+/*     */
+/*     let i = 10;  */
+/*  */
+/*    while(i <= numeros || i === 10) { */
+/*        console.log(i);  */
+/*        i++;  */
+/*    } */
+/* } */
+/*  */
+/* threeBucle(20);  */
 
-    let i = 0; 
+              //2) ==== DoWhile
 
-    while(i <= num) {
-     console.log(i); 
-     i++; 
-    }
-}
-bucleWhile(15); 
+/* doFunction = (num) => { */
+/*  */
+/*  let i = 0;  */
+/*  */
+/*  do { */
+/*      console.log(i);  */
+/*      i++;  */
+/*  }while(i < num);   */
+/*  */
+/* } */
+/*  */
+/* doFunction(10);  */
 
-console.log("Two"); 
+/* twoWhile = (numero) => { */
+/*    */
+/*   let i = 5;  */
+/*  */
+/*   do { */
+/*       console.log(i);  */
+/*       i++;  */
+/*   } while (i <= numero || numero === 15); */
+/* } */
+/*  */
+/* twoWhile(15);  */
+/*  */
 
-twoBucle = (data) => {
- 
-    let i = 5; 
+/* twoFunction = (datos) => { */
+/*     let i = 5;  */
+/*  */
+/*     do { */
+/*         console.log(i);  */
+/*         i++;  */
+/*     } while (i < datos); */
+/* } */
+/*  */
+/* twoFunction(15);  */
 
-    while(i < data) {
+    /*3) ===== CICLO FOR */
+// FOR(inicialización, condicional, actualización) {}
+
+oneBucle = (num) =>  {
+  
+    for(let i = 0; i <= num; i++ ) {
         console.log(i); 
-        i++; 
     }
 }
-twoBucle(15); 
 
-console.log("Three"); 
-
-threeBucle = (numeros) => {
-   
-    let i = 10; 
-
-   while(i <= numeros || i === 10) {
-       console.log(i); 
-       i++; 
-   }
-}
-
-threeBucle(20); 
+oneBucle(11); 
