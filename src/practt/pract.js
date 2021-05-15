210001d9e5114e3ed5ff55a0ddfaff0be6ed7f15
Christiano0407 <placebo0704@gmail.com>
2021-05-15 10:07:05 -0500

    bucleTwo

--- src/practt/pract.js
+++ src/practt/pract.js
@@ -1,1 +1,41 @@
 console.log("Hello World"); 
+
+// Bucles - Js
+
+function bucleWhile(num) {
+
+    let i = 0; 
+
+    while(i <= num) {
+     console.log(i); 
+     i++; 
+    }
+}
+bucleWhile(15); 
+
+console.log("Two"); 
+
+twoBucle = (data) => {
+ 
+    let i = 5; 
+
+    while(i < data) {
+        console.log(i); 
+        i++; 
+    }
+}
+twoBucle(15); 
+
+console.log("Three"); 
+
+threeBucle = (numeros) => {
+   
+    let i = 10; 
+
+   while(i <= numeros || i === 10) {
+       console.log(i); 
+       i++; 
+   }
+}
+
+threeBucle(20); 
